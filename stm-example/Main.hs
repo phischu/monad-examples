2c15c35ba55888fb38df015bdf7dea5d9b19a5e1
Philipp Schuster <phischu3000@googlemail.com>
2015-08-18 20:21:16 +0200

    stm-example

---
+++ stm-example/Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import Control.Concurrent.STM (
+    STM, TVar, readTVar, writeTVar,
+    atomically, newTVarIO, readTVarIO)
+import Control.Concurrent (
+    forkIO, threadDelay)
+
+swapTVars :: TVar Int -> TVar Int -> STM ()
+swapTVars tvar1 tvar2 = do
+
+    value1 <- readTVar tvar1
+    value2 <- readTVar tvar2
+    
+    writeTVar tvar1 value2
+    writeTVar tvar2 value1
+
+main :: IO ()
+main = do
+
+    tvar1 <- newTVarIO 1
+    tvar2 <- newTVarIO 2
+
+    forkIO (atomically (swapTVars tvar1 tvar2))
+    forkIO (atomically (swapTVars tvar1 tvar2))
+
+    threadDelay 1000000
+
+    value1 <- readTVarIO tvar1
+    value2 <- readTVarIO tvar2
+
+    putStrLn ("value1: " ++ show value1)
+    putStrLn ("value2: " ++ show value2)

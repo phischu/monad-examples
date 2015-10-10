f40e24f55163130a25ee6644fc2b980f5fe07f6e
Philipp Schuster <phischu3000@googlemail.com>
2015-10-10 14:22:07 +0200

    Make resourcet example compile.

--- resourcet-example/Main.hs
+++ resourcet-example/Main.hs
@@ -1,22 +1,9 @@
 module Main where
 
 
+import Control.Monad.Trans.Resource (runResourceT)
 
-type Username = Text
-
-type Highscore = Integer
-
-selectUsers :: Connection -> IO [(Username,Highscore)]
-selectUsers connection =
-    query_ connection "SELECT * FROM users"
-
-insertUser :: Connection -> (Username,Highscore) -> IO ()
-insertUser connection =
-    execute_ connection "INSERT INTO users (?,?)"
 
 main :: IO ()
 main = runResourceT (do
-    mydb <- allocateSQLite "my.db"
-    memorydb <- allocateSQLite ":memory:"
     return ())
-

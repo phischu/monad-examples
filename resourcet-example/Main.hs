52665a7374bc3d2ad9e1cdca2a7be61bb7b36ac7
Philipp Schuster <phischu3000@googlemail.com>
2016-07-26 11:22:50 +0200

    ResourceT example.

--- resourcet-example/Main.hs
+++ resourcet-example/Main.hs
@@ -1,9 +1,59 @@
 module Main where
 
 
-import Control.Monad.Trans.Resource (runResourceT)
+import Control.Monad.Trans.Resource (
+  ResIO, runResourceT, allocate, ReleaseKey)
+
+import System.IO (
+  openFile, IOMode(AppendMode), Handle,
+  hPutStrLn, hFlush,
+  hClose)
+
+import Control.Monad.IO.Class (
+  liftIO)
+
+
+allocateFile :: FilePath -> IOMode -> ResIO (ReleaseKey, Handle)
+allocateFile filename ioMode = allocate openAction closeAction where
+
+  openAction = do
+    putStrLn ("Opening " ++ filename)
+    openFile filename ioMode
+
+  closeAction handle = do
+    putStrLn ("Closing " ++ filename)
+    hClose handle
+
+
+askForFile :: ResIO Handle
+askForFile = do
+
+  liftIO (putStrLn "Enter filename:")
+  filename <- liftIO getLine
+
+  (_, handle) <- allocateFile filename AppendMode
+  return handle
+
+
+loop :: Handle -> ResIO ()
+loop handle = do
+
+  liftIO (putStrLn "Appending to file. Enter stop to stop:")
+  userInput <- liftIO getLine
+
+  case userInput of
+
+    "stop" -> do
+      return ()
+
+    _ -> do
+      liftIO (hPutStrLn handle userInput)
+      liftIO (hFlush handle)
+      loop handle
 
 
 main :: IO ()
 main = runResourceT (do
-    return ())
+  handle <- askForFile
+  loop handle)
+

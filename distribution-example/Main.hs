c4df84778db76af7062e8892e550d5872da3adfb
Philipp Schuster <phischu3000@googlemail.com>
2016-07-27 17:03:58 +0200

    Reformat distribution example.

--- distribution-example/Main.hs
+++ distribution-example/Main.hs
@@ -1,36 +1,42 @@
 module Main where
 
+
 import Numeric.Probability.Distribution (
-    T, uniform, norm, pretty)
+  T, uniform, norm, pretty)
+
 
 type Distribution a = T Rational a
+
 
 sixSided :: Distribution Int
 sixSided = uniform [1..6]
 
+
 twentySided :: Distribution Int
 twentySided = uniform [1..20]
 
+
 youRoll :: Distribution Int
 youRoll = norm (do
-    firstRoll <- sixSided
-    secondRoll <- sixSided
-    return (firstRoll + secondRoll + 5))
+  firstRoll <- sixSided
+  secondRoll <- sixSided
+  return (firstRoll + secondRoll + 5))
+
 
 enemyRolls :: Distribution Int
 enemyRolls = norm (do
-    firstRoll <- twentySided
-    secondRoll <- twentySided
-    return (max firstRoll secondRoll))
+  firstRoll <- twentySided
+  secondRoll <- twentySided
+  return (max firstRoll secondRoll))
+
 
 youWin :: Distribution Bool
 youWin = norm (do
-    yourScore <- youRoll
-    enemyScore <- enemyRolls
-    return (yourScore > enemyScore))
+  yourScore <- youRoll
+  enemyScore <- enemyRolls
+  return (yourScore > enemyScore))
+
 
 main :: IO ()
 main = putStrLn (pretty show youWin)
-
-
 

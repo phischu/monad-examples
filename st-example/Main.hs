b62be93ca1f211a90076d2dffb6ecec1e9f8debf
Philipp Schuster <phischu3000@googlemail.com>
2016-07-30 11:04:34 +0200

    Upgrade ST example.

--- st-example/Main.hs
+++ st-example/Main.hs
@@ -1,26 +1,36 @@
 module Main where
 
-import Prelude hiding (
-    length)
 import Data.Vector.Unboxed (
-    Vector, create, (!), length, fromList, forM_)
+  Vector)
+import qualified Data.Vector.Unboxed as Vector (
+  create, fromList, forM_)
 import Data.Vector.Unboxed.Mutable (
-    MVector, new, modify)
+  MVector)
+import qualified Data.Vector.Unboxed.Mutable as MVector (
+  new, modify)
 import Control.Monad.ST (
-    ST)
+  ST)
 import Data.Word (
-    Word8)
+  Word8)
+
 
 histogram :: Vector Word8 -> Vector Int
-histogram vector = create (histogramST vector)
+histogram vector = Vector.create (histogramST vector)
+
 
 histogramST :: Vector Word8 -> ST s (MVector s Int)
 histogramST vector = do
-    resultVector <- new 256
-    forM_ vector (\value -> do
-        modify resultVector (+1) (fromIntegral value))
-    return resultVector
+
+  resultVector <- MVector.new 256
+
+  Vector.forM_ vector (\value -> do
+
+    MVector.modify resultVector (+1) (fromIntegral value))
+
+  return resultVector
+
 
 main :: IO ()
 main = do
-    print (histogram (fromList [5,6,2,8,8]))
+  print (histogram (Vector.fromList [5,6,2,8,8]))
+
